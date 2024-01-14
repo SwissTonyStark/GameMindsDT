8452830e766003035e19f9f9e1019286f57e3101
Drinf <eplanellv@gmail.com>
2024-01-14 23:24:02 +0100

    Test door key

--- experiments/notebooks/test_human.py
+++ experiments/notebooks/test_human.py
@@ -3,7 +3,7 @@ from gym.utils.play import play
 import numpy as np
 
 
-play(gym.make("MiniGrid-DoorKey-5x5-v0", render_mode="rgb_array"), keys_to_action={  
+play(gym.make("MiniGrid-DoorKey-16x16-v0", render_mode="rgb_array"), keys_to_action={  
                                                "w": np.array([2]),
                                                "a": np.array([0]),
                                                "d": np.array([1]),
