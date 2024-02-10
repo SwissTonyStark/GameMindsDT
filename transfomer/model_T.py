2c1f712f1dbdefce2c388db41b260761e9b1eacf
shuang <shuanglongjq@gmail.com>
2024-02-10 10:14:49 +0100

    files

--- transfomer/model_T.py
+++ transfomer/model_T.py
@@ -17,37 +17,37 @@ class MaskedSelfAttention(nn.Module):
 
         self.embed_dim = embed_dim # embeding dimensionality, includes all heads
         self.num_heads = num_heads #  num heads
-        assert self.n_embd % self.n_head == 0 , \
+        assert self.embed_dim % self.num_heads == 0 , \
             "Embedding dimension must be multiple of the number of heads."
-        
+
         self.seq_len = seq_len
 
-        # key, query, value projections 
+        # key, query, value projections
         self.proj_q = nn.Linear(embed_dim, embed_dim)
         self.proj_k = nn.Linear(embed_dim, embed_dim)
         self.proj_v = nn.Linear(embed_dim, embed_dim)
 
         # output projection
-        self.proj_out = nn.Linear(self.n_embd, self.n_embd)
+        self.proj_out = nn.Linear(self.embed_dim, self.embed_dim)
         # regularization
         self.attn_dropout = nn.Dropout(dropout)
         self.resid_dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        B, T, C = x.shape # batch size, sequence length, embedding dimensionality (n_embd)
-        #head_size = self.n_head, C // self.n_head
+        B, T, C = x.shape # batch size, sequence length, embedding dimensionality (embed_dim)
+        #head_size = self.num_heads, C // self.num_heads
 
         # calculate query, key, values
-        q = self.proj_q(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
-        k = self.proj_k(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
-        v = self.proj_v(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
+        q = self.proj_q(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
+        k = self.proj_k(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
+        v = self.proj_v(x).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2) # (B, seqLen, numHeads, headSize) -> (B, numHeads, seqLen, headSize)
 
         # causal self-attention; Self-attend: (B, numHeads, seqLen, headSize) x (B, numHeads, headSize, seqLen) -> (B, numHeads, seqLen, seqLen)
         # scaled_dot_product
         attn_logits = (q @ k.transpose(-2, -1))
-        attn_logits = attn_logits / torch.sqrt(torch.tensor(k.size(-1))) 
+        attn_logits = attn_logits / torch.sqrt(torch.tensor(k.size(-1)))
         # apply mask
-       
+
         mask = torch.zeros(x.shape[1], x.shape[0]).bool() #toDevice
         subsequent_mask = torch.triu(torch.ones(B, T, T), 1).bool() #toDevice
         selfattn_mask = subsequent_mask + mask.unsqueeze(-2)
@@ -84,13 +84,13 @@ class MLP(nn.Module):
 
 class DecoderBlock(nn.Module):
 
-    def __init__(self, embed_dim, num_heads, seq_len, dropout, ffn_ratio=4):
+    def __init__(self, embed_dim, num_heads, seq_len, mlp_ratio, dropout):
         super().__init__()
 
         self.attn = MaskedSelfAttention(embed_dim, num_heads, seq_len, dropout)
         self.ln1 = nn.LayerNorm(embed_dim)
-        self.mlp = MLP(embed_dim, int(embed_dim * ffn_ratio))
-        self.ln_2 = nn.LayerNorm(self.embed_dim)
+        self.mlp = MLP(embed_dim, int(embed_dim * mlp_ratio),dropout)
+        self.ln_2 = nn.LayerNorm(embed_dim)
 
     def forward(self, x):
         x = self.ln1(x) # normalize
