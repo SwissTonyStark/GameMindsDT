5d0245aff3c20deb0d020ffdf854d06bfbd11612
Omar A. <omar.in_95@hotmail.com>
2024-03-09 17:52:49 +0100

    Updating readme

--- DT-atari/code/train_.sh
+++
@@ -1,5 +0,0 @@
-
-python main.py --game_name='qbert-expert-v4' --act_voc=6 --context_length=30 --batch_size=128 --target_reward=2500
-#python main.py --game_name='breakout-mixed-v4' --act_voc=4 --context_length=30 --batch_size=128 --target_reward=90
-#python main.py --game_name='space-invaders-expert-v4' --act_voc=6 --context_length=30 --batch_size=128 --target_reward=20000
-#python main.py --game_name='seaquest-expert-v4' --act_voc=18 --context_length=30 --batch_size=128 --target_reward=1450
